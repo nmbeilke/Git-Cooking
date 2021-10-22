e8a84b7dba69623858f3447c2448ca3cee92ed8d
Edgar Fernandez <Edgar.Fernand@outlook.com>
2021-10-22 12:30:58 -0700

    Added a bit on the main part of the website. Not yet complete.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,3 +20,33 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 //-----------------------------------------/end js for Scroll to Top Page Button----------------------------------
+
+
+//-----------------------------------------js for collaspable nav bar menu----------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+//-----------------------------------------/end js for collaspable nav bar menu----------------------------------
+
